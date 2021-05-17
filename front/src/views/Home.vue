5e4e8ecf2ab0c3e5f52e097d74a8e76abd92ffb8
Yeseo Lee <s2019w35@e-mirim.hs.kr>
2021-05-17 16:19:17 +0900

    Vue & Express install

---
+++ front/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <button @click="foo">submit</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    foo(){
+      this.$http.get('/users')
+    }
+  },
+}
+</script>
