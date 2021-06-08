46be90ac386c03b3e1ded8c83a4aab47cd0d21a2
jiyujung <s2019w39@e-mirim.hs.kr>
2021-06-09 02:04:36 +0900

    add login

--- back/index.js
+++ back/index.js
@@ -1,9 +1,11 @@
 var express   = require('express');
 var app       = express();
+const path = require('path')
 var passport  = require('passport');
 var session   = require('express-session');
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 app.use(session({secret:'MySecret', resave: false, saveUninitialized:true}));
 
 // Passport setting
@@ -18,4 +20,4 @@ app.use('/auth', require('./routes/auth'));
 var port = 3000;
 app.listen(port, function(){
   console.log('server on! http://localhost:'+port);
-});
+});``
--- back/routes/main.js
+++ back/routes/main.js
@@ -5,4 +5,35 @@ router.get('/', function(req,res){
   res.render('main', {user: req.user});
 });
 
+router.get('/recommend.ejs', function(req,res){
+  res.render('recommend', {user: req.user});
+});
+
+router.get('/bathandbody.ejs', function(req,res){
+  res.render('bathandbody', {user: req.user});
+});
+
+router.get('/fragrance.ejs', function(req,res){
+  res.render('fragrance', {user: req.user});
+});
+
+router.get('/mypage.ejs', function(req,res){
+  res.render('mypage', {user: req.user});
+});
+
+router.get('/list.ejs', function(req,res){
+  res.render('list', {user: req.user});
+});
+
+router.get('/product.ejs', function(req,res){
+  res.render('product', {user: req.user});
+});
+
+router.get('/homefragrance.ejs', function(req,res){
+  res.render('homefragrance', {user: req.user});
+});
+
+router.get('/search.ejs', function(req,res){
+  res.render('search', {user: req.user});
+});
 module.exports = router;
