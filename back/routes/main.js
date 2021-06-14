bf4f6843108d5b972d0df8039c318380218a94d7
jiyujung <s2019w39@e-mirim.hs.kr>
2021-06-15 00:33:02 +0900

    complete login

--- back/routes/main.js
+++ back/routes/main.js
@@ -5,35 +5,36 @@ router.get('/', function(req,res){
   res.render('main', {user: req.user});
 });
 
-router.get('/recommend.ejs', function(req,res){
-  res.render('recommend', {user: req.user});
+router.get('/recommend', function(req,res){
+  res.render('recommend.ejs', {user: req.user});
 });
 
-router.get('/bathandbody.ejs', function(req,res){
-  res.render('bathandbody', {user: req.user});
+router.get('/bathandbody', function(req,res){
+  res.render('bathandbody.ejs', {user: req.user});
 });
 
-router.get('/fragrance.ejs', function(req,res){
-  res.render('fragrance', {user: req.user});
+router.get('/fragrance', function(req,res){
+  res.render('fragrance.ejs', {user: req.user});
 });
 
-router.get('/mypage.ejs', function(req,res){
-  res.render('mypage', {user: req.user});
+router.get('/mypage', function(req,res){
+  res.render('mypage.ejs', {user: req.user});
 });
 
-router.get('/list.ejs', function(req,res){
-  res.render('list', {user: req.user});
+router.get('/list', function(req,res){
+  res.render('list.ejs', {user: req.user});
 });
 
-router.get('/product.ejs', function(req,res){
-  res.render('product', {user: req.user});
+router.get('/product', function(req,res){
+  res.render('product.ejs', {user: req.user});
 });
 
-router.get('/homefragrance.ejs', function(req,res){
-  res.render('homefragrance', {user: req.user});
+router.get('/homefragrance', function(req,res){
+  res.render('homefragrance.ejs', {user: req.user});
 });
 
-router.get('/search.ejs', function(req,res){
-  res.render('search', {user: req.user});
+router.get('/search', function(req,res){
+  res.render('search.ejs', {user: req.user});
 });
+
 module.exports = router;
