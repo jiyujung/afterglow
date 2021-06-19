3f8caca61701d7943b042329e4cc5ae5f4cafeef
jiyujung <s2019w39@e-mirim.hs.kr>
2021-06-19 22:31:48 +0900

    add list

--- back/routes/fragrance.js
+++ back/routes/fragrance.js
@@ -18,13 +18,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/list/:page', function(req, res, next) {
+  var page = req.params.page;
     pool.getConnection(function(err, connection) {
         var sqlForSelectList = "SELECT p_name, p_price, p_brand, p_img FROM product WHERE p_category=1";
         connection.query(sqlForSelectList, function(err, rows) {
             if(err) console.error("err: " + err);
             console.log("rows:" + JSON.stringify(rows));
 
-            res.render('fragrance', {rows:rows});
+            res.render('fragrance', {rows:rows, page:page, length:rows.length-1, page_num:12, pass:true});
+            console.log(rows.length-1);
             connection.release;
         })
     })
