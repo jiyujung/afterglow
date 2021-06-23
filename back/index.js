e4af0ee86ac6b9420867f6136389d16e5f942c16
jiyujung <s2019w39@e-mirim.hs.kr>
2021-06-24 00:03:14 +0900

    edit search

--- back/index.js
+++ back/index.js
@@ -3,6 +3,7 @@ var app       = express();
 const path = require('path')
 var passport  = require('passport');
 var session   = require('express-session');
+var bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +12,11 @@ app.use(session({secret:'MySecret', resave: false, saveUninitialized:true}));
 // Passport setting
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Routes
 app.use('/', require('./routes/main'));
--- back/routes/fragrance.js
+++ back/routes/fragrance.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('./db.js');
+var url = require('url');
+const search = "";
 
 router.get('/', function(req, res, next) {
     res.redirect('/fragrance/list/1');
@@ -20,6 +22,25 @@ router.get('/list/:page', function(req, res, next) {
         })
     })
 })
+
+router.get('/list/search/:page',function(req,res,next) {
+  var page = req.params.page;
+  var _url = req.url;
+  var queryData = url.parse(_url,true).query;
+  var search = '%' + queryData.fragrance + '%';
+  pool.getConnection(function(err, connection) {
+    var sql = "SELECT * FROM product WHERE p_category=1 and (p_brand like ? or p_name like ? or p_note like ?)";
+    connection.query(sql, [search, search, search], function(err,rows) {
+      if(err) console.error(err);
+      
+      console.log("rows:" + search);
+
+      res.render('fragrance', {rows:rows, page:page, length:rows.length-1, page_num:12, pass:true});
+      connection.release;
+      
+    });
+  })
+});
 
 router.get('/list/product/:p_id',function(req,res,next) {
   var p_id = req.params.p_id;
